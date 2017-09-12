2d19a4ff7a20d4f187ef626a2391c71d49e6fc41
Annie Tang <annietang@berkeley.edu>
2017-09-12 15:40:00 -0700

    updated testHM to be without junit and created TestHMAssert for with junit

--- TestHM.java
+++ TestHM.java
@@ -1,5 +1,7 @@
-import static org.junit.Assert.*;
-import org.junit.Test;
+/**
+ * Initially used junit testing & assertEquals to properly unit test,
+ * but since we can't use external libaries, I decided to change all of it to print statements!
+ */
 
 //constructor(size): return an instance of the class with pre-allocated space for the given number of objects.
 //boolean set(key, value): stores the given key/value pair in the hash map. Returns a boolean value indicating success / failure of the operation.
@@ -8,43 +10,44 @@ import org.junit.Test;
 //float load(): return a float value representing the load factor (`(items in hash map)/(size of hash map)`) of the data structure. Since the size of the dat structure is fixed, this should never be greater than 1.
 
 public class TestHM {
-    public FixedHashMap testInt= new FixedHashMap(3);
-    public FixedHashMap testStr = new FixedHashMap(10);
+    public static FixedHashMap testInt= new FixedHashMap(3);
+    public static FixedHashMap testStr = new FixedHashMap(10);
 
-    public void setUp() {
-        assertEquals(testInt.set("First", 1), true);
-        assertEquals(testInt.set("Second", 2), true);
-        assertEquals(testInt.set("Third", 3), true);
-        assertEquals(testInt.set("Fourth", 3), false);
-        assertEquals(testStr.set("01", "serendipity"), true);
-        assertEquals(testStr.set("02", "courage"), true);
-        assertEquals(testStr.set("03", "laundry"), true);
+    public static void setUp() {
+        System.out.println(testInt.set("First", 1));
+        System.out.println(testInt.set("Second", 2));
+        System.out.println(testInt.set("Third", 3));
+        System.out.println(testInt.set("Fourth", 3));
+        System.out.println(testStr.set("01", "serendipity"));
+        System.out.println(testStr.set("02", "courage"));
+        System.out.println(testStr.set("03", "laundry"));
     }
 
-    @Test
-    public void testGet() {
+    public static void testGet() {
         setUp();
-        assertEquals(testInt.get("First"), 1);
-        assertEquals(testInt.get("Second"), 2);
-        assertEquals(testInt.get("Third"), 3);
-        assertEquals(testInt.get("Fourth"), null);
-        assertEquals(testStr.get("03"), "laundry");
+        System.out.println(testInt.get("First"));
+        System.out.println(testInt.get("Second"));
+        System.out.println(testInt.get("Third"));
+        System.out.println(testInt.get("Fourth"));
+        System.out.println(testStr.get("03"));
 
     }
 
-    @Test
-    public void testDelete() {
+    public static void testDelete() {
         setUp();
-        assertEquals(testStr.load(), 0.3, 1);
-        assertEquals(testInt.load(), 1, 0);
-        assertEquals(testInt.delete("First"), 1);
-        assertEquals(testInt.delete("Second"), 2);
-        assertEquals(testInt.delete("Fourth"), null);
-        assertEquals(testStr.delete("03"), "laundry");
-        assertEquals(testStr.delete("02"), "courage");
+        System.out.println(testStr.load());
+        System.out.println(testInt.load());
+        System.out.println(testInt.delete("First"));
+        System.out.println(testInt.delete("Second"));
+        System.out.println(testInt.delete("Fourth"));
+        System.out.println(testStr.delete("03"));
+        System.out.println(testStr.delete("02"));
     }
 
 
     public static void main(String[] args) {
+        setUp();
+        testGet();
+        testDelete();
     }
 }
---
+++ TestHMAssert.java
@@ -0,0 +1,50 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+//constructor(size): return an instance of the class with pre-allocated space for the given number of objects.
+//boolean set(key, value): stores the given key/value pair in the hash map. Returns a boolean value indicating success / failure of the operation.
+//get(key): return the value associated with the given key, or null if no value is set.
+//delete(key): delete the value associated with the given key, returning the value on success or null if the key has no value.
+//float load(): return a float value representing the load factor (`(items in hash map)/(size of hash map)`) of the data structure. Since the size of the dat structure is fixed, this should never be greater than 1.
+
+public class TestHMAssert {
+    public FixedHashMap testInt= new FixedHashMap(3);
+    public FixedHashMap testStr = new FixedHashMap(10);
+
+    public void setUp() {
+        assertEquals(testInt.set("First", 1), true);
+        assertEquals(testInt.set("Second", 2), true);
+        assertEquals(testInt.set("Third", 3), true);
+        assertEquals(testInt.set("Fourth", 3), false);
+        assertEquals(testStr.set("01", "serendipity"), true);
+        assertEquals(testStr.set("02", "courage"), true);
+        assertEquals(testStr.set("03", "laundry"), true);
+    }
+
+    @Test
+    public void testGet() {
+        setUp();
+        assertEquals(testInt.get("First"), 1);
+        assertEquals(testInt.get("Second"), 2);
+        assertEquals(testInt.get("Third"), 3);
+        assertEquals(testInt.get("Fourth"), null);
+        assertEquals(testStr.get("03"), "laundry");
+
+    }
+
+    @Test
+    public void testDelete() {
+        setUp();
+        assertEquals(testStr.load(), 0.3, 1);
+        assertEquals(testInt.load(), 1, 0);
+        assertEquals(testInt.delete("First"), 1);
+        assertEquals(testInt.delete("Second"), 2);
+        assertEquals(testInt.delete("Fourth"), null);
+        assertEquals(testStr.delete("03"), "laundry");
+        assertEquals(testStr.delete("02"), "courage");
+    }
+
+
+    public static void main(String[] args) {
+    }
+}
